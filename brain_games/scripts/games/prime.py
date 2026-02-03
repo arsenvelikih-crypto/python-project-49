3bb94e2345e804e1294193590130413edc9aac25
arsenvelikih-crypto <arsenvelikih@gmail.com>
2026-02-03 22:08:13 +0400

    update prime

--- brain_games/scripts/games/prime.py
+++ brain_games/scripts/games/prime.py
@@ -3,11 +3,18 @@ from random import randint
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def get_question_and_answer():
+def is_prime(n):
+    if n < 2:
+        return False
+    
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
 
+
+def get_question_and_answer():
     num = randint(2, 100)
-    item = [2, 3, 5]
-    for i in item:
-        if num != i and num % i == 0:
-            return num, 'no'
-    return num, 'yes'
+    answer = 'yes' if is_prime(num) else 'no'
+    return num, answer
